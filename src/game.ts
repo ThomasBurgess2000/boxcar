1110839d37e02451f814b0ad38481b76f9f2ac5a
Thomas Burgess <thomasburgess2000@gmail.com>
2023-12-20 01:19:18 -0600

    gotta go ahead and commit this so I can use the asset in the playground, but it's not cleaned up yet

--- src/game.ts
+++ src/game.ts
@@ -7,16 +7,16 @@ import {
   Color3,
   Color4,
   Engine,
-  HemisphericLight,
   Material,
   Mesh,
+  PBRMaterial,
+  PointLight,
   Scene,
   SceneLoader,
   ShaderLanguage,
   ShaderMaterial,
   ShaderStore,
   Texture,
-  UniversalCamera,
   Vector3,
 } from '@babylonjs/core';
 import { Inspector } from '@babylonjs/inspector';
@@ -30,6 +30,7 @@ export async function startGame() {
   ShaderStore.ShadersStore['customVertexShader'] = `
     attribute vec3 position;
     attribute vec2 uv;
+    attribute vec3 normal;
 
     #include<instancesDeclaration>
     uniform mat4 view;
@@ -39,6 +40,8 @@ export async function startGame() {
     uniform mat4 projection;
 
     varying vec2 vUV;
+    varying vec3 vPositionW;
+    varying vec3 vNormalW;
 
     float inverseLerp(float v, float minValue, float maxValue) {
       return (v - minValue) / (maxValue - minValue);
@@ -62,6 +65,11 @@ export async function startGame() {
         vertexOffset = mix(vertexOffset, normalize(vertexOffset), u_normalize);
       }
 
+      vec4 worldPosition = finalWorld * vec4(position, 1.0);
+      vPositionW = worldPosition.xyz;
+
+      vNormalW = normalize(vec3(finalWorld * vec4(normal, 0.0)));
+
       vec4 worldViewPosition = view * finalWorld * vec4(position, 1.0);
 
       worldViewPosition += vec4(mix(vec3(0.0), vec3(vertexOffset, 1.0), u_effectBlend), 0.0);
@@ -73,18 +81,54 @@ export async function startGame() {
   `;
 
   ShaderStore.ShadersStore['customFragmentShader'] = `
-    precision highp float;
-    varying vec2 vUV;
-    uniform sampler2D textureSampler;
-    uniform vec3 u_color;
-    void main(void) {
-      vec4 texColor = texture2D(textureSampler, vUV);
-      float luminance = dot(texColor.rgb, vec3(0.299, 0.587, 0.114));
-      gl_FragColor = vec4(u_color * texColor.rgb, luminance);
-      if (luminance < 0.75) {
-        discard;
+  precision highp float;
+
+  // Varying variables for lighting calculations
+  varying vec2 vUV;
+  varying vec3 vPositionW;
+  varying vec3 vNormalW;
+
+  // Uniforms
+  uniform sampler2D textureSampler;
+  uniform vec3 u_color;
+  uniform vec3 vLightPosition; // Add a light position uniform
+
+  void main(void) {
+      // Toon shader thresholds and brightness levels
+      float ToonThresholds[4];
+      ToonThresholds[0] = 0.95;
+      ToonThresholds[1] = 0.5;
+      ToonThresholds[2] = 0.2;
+      ToonThresholds[3] = 0.03;
+
+      float ToonBrightnessLevels[5];
+      ToonBrightnessLevels[0] = 1.0;
+      ToonBrightnessLevels[1] = 0.8;
+      ToonBrightnessLevels[2] = 0.6;
+      ToonBrightnessLevels[3] = 0.35;
+      ToonBrightnessLevels[4] = 0.2;
+
+      // Light calculation
+      vec3 lightVectorW = normalize(vPositionW - vLightPosition);
+      float ndl = max(0., dot(vNormalW, lightVectorW));
+
+      // Apply toon shading
+      vec3 color = texture2D(textureSampler, vUV).rgb;
+      for (int i = 0; i < 4; i++) {
+          if (ndl > ToonThresholds[i]) {
+              color *= ToonBrightnessLevels[i];
+              break;
+          }
       }
-    }
+
+      // Original luminance and transparency logic
+      float luminance = dot(color, vec3(0.299, 0.587, 0.114));
+      if (luminance < 0.75) {
+          discard;
+      }
+
+      gl_FragColor = vec4(u_color * color, luminance);
+  }
   `;
 
   // Create canvas and engine
@@ -97,12 +141,13 @@ export async function startGame() {
   // Create a basic scene
   const scene = new Scene(engine);
   scene.useRightHandedSystem = true;
-  scene.clearColor = Color4.FromColor3(Color3.White());
-  Inspector.Show(scene, {});
+  scene.clearColor = Color4.FromColor3(Color3.Black());
+  // Inspector.Show(scene, {});
 
-  const camera = new ArcRotateCamera('camera', 0, 0, 0, new Vector3(0, 0, 0), scene);
+  const camera = new ArcRotateCamera('camera', 0, 0, 0, new Vector3(0, 2, 0), scene);
   camera.attachControl(canvas, true);
-  const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
+  // const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
+  const pointLight = new PointLight('pointLight', new Vector3(0, 20, 10), scene);
 
   engine.runRenderLoop(() => {
     scene.render();
@@ -117,7 +162,7 @@ export async function startGame() {
   // const trackSections = ['straight', 'left', 'right', 'left', 'straight'];
   const trackSections = ['straight', 'left'];
   const trackComponent = createTrack(trackSections);
-  createLocomotive(trackComponent);
+  // createLocomotive(trackComponent);
   makeTree(scene);
 }
 
@@ -146,7 +191,7 @@ function addCurveSection(points: Vector3[], turnDirection: 'left' | 'right', tur
     let dz = r * (1 - Math.cos(angle));
 
     // flip the direction if turning right
-    if (turnDirection === 'right') {
+    if (turnDirection === 'left') {
       dz = -dz;
     }
 
@@ -230,8 +275,8 @@ async function makeTree(scene: Scene) {
       scene,
       { vertex: 'custom', fragment: 'custom' },
       {
-        attributes: ['position', 'uv'],
-        uniforms: ['world', 'view', 'projection'],
+        attributes: ['position', 'uv', 'normal'],
+        uniforms: ['view', 'projection', 'vLightPosition', 'u_color'],
         uniformBuffers: undefined,
         shaderLanguage: ShaderLanguage.GLSL,
       },
@@ -253,13 +298,22 @@ async function makeTree(scene: Scene) {
     shaderMaterial.setFloat('u_remap', 1.0);
     shaderMaterial.setFloat('u_normalize', 1.0);
     shaderMaterial.setFloat('u_effectBlend', 1.0);
+    shaderMaterial.setVector3('vLightPosition', new Vector3(0, 20, 10));
 
     // find the child mesh with the name 'trunk'
     const trunk = tree.getChildMeshes().find((mesh) => mesh.name === 'trunk');
     if (!trunk || !trunk.material) {
       return;
     }
-    trunk.material.disableDepthWrite = true;
+    const trunkPBRMaterial = new PBRMaterial('trunkPBRMaterial', scene);
+
+    // brown
+    trunkPBRMaterial.emissiveColor = new Color3(0.3, 0.2, 0.1);
+    trunkPBRMaterial.emissiveIntensity = 0.13;
+    trunkPBRMaterial.metallic = 0.1; // Low metallic value for non-metal surfaces
+    trunkPBRMaterial.roughness = 0.8; // Increase roughness to reduce shininess
+
+    trunk.material = trunkPBRMaterial;
 
     // find the child mesh with the name 'foliage'
     const foliage = tree.getChildMeshes().find((mesh) => mesh.name === 'foliage');
@@ -267,7 +321,6 @@ async function makeTree(scene: Scene) {
       return;
     }
     foliage.material = shaderMaterial;
-    shaderMaterial.setMatrix('world', foliage.getWorldMatrix());
   });
 
   // make instance of tree
@@ -291,5 +344,5 @@ function createTreeInstance(scene: Scene) {
   foliageInstance.parent = treeInstance;
   trunkInstance.parent = treeInstance;
 
-  treeInstance.position = new Vector3(0, 0, 5);
+  treeInstance.position = new Vector3(7, 0, 7);
 }
