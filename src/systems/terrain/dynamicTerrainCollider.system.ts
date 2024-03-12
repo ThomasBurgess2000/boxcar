9eaf77e10843e958749923df24e17e4684f43e89
Thomas Burgess <thomasburgess2000@gmail.com>
2024-03-12 03:47:43 -0500

    By golly it works

--- src/systems/terrain/dynamicTerrainCollider.system.ts
+++ src/systems/terrain/dynamicTerrainCollider.system.ts
@@ -2,20 +2,10 @@ import { Entity, IterativeSystem } from 'tick-knock';
 import { DynamicTerrainComponent } from '../../components/dynamicTerrain.component';
 import { RegisterSystem } from '../../startup/systemRegistration';
 import { InitializationStatus } from '../../utils/types';
-import {
-  ArcRotateCamera,
-  Mesh,
-  MeshBuilder,
-  PhysicsAggregate,
-  PhysicsShapeType,
-  SubMesh,
-  Vector3,
-  VertexBuffer,
-  VertexData,
-} from '@babylonjs/core';
-import { MAX_VIEW_DISTANCE, scene } from '../../game';
+import { Mesh, MeshBuilder, PhysicsAggregate, PhysicsShapeType, Vector3, VertexBuffer, VertexData } from '@babylonjs/core';
+import { scene } from '../../game';
 
-const RADIUS_OF_PHYSICS_AGGREGATE = 30;
+const DIAMETER_OF_PHYSICS_AGGREGATE = 30;
 
 @RegisterSystem()
 export class DynamicTerrainColliderSystem extends IterativeSystem {
@@ -36,16 +26,15 @@ export class DynamicTerrainColliderSystem extends IterativeSystem {
       return;
     }
 
-    const camera = scene.activeCamera as ArcRotateCamera;
-
-    if (!camera) {
+    const locomotive = scene.getMeshByName('locomotive');
+    if (!locomotive) {
       return;
     }
 
     if (!dynamicTerrainComponent.physicsMesh && !dynamicTerrainComponent.physicsAggregateInitializing) {
       console.log('Creating physics aggregate', dynamicTerrainComponent.physicsMesh, dynamicTerrainComponent.physicsAggregateInitializing);
       dynamicTerrainComponent.physicsAggregateInitializing = true;
-      const result = this.createPhysicsAggregate(terrainMesh, camera.globalPosition.clone(), RADIUS_OF_PHYSICS_AGGREGATE);
+      const result = this.createPhysicsAggregate(terrainMesh, locomotive.getAbsolutePosition().clone(), DIAMETER_OF_PHYSICS_AGGREGATE);
       dynamicTerrainComponent.physicsMesh = result.physicsMeshClone;
       dynamicTerrainComponent.currentPhysicsCenter = result.centerPosition;
       dynamicTerrainComponent.physicsAggregateInitializing = false;
@@ -54,23 +43,25 @@ export class DynamicTerrainColliderSystem extends IterativeSystem {
       !dynamicTerrainComponent.physicsAggregateInitializing &&
       dynamicTerrainComponent.currentPhysicsCenter
     ) {
-      const center = dynamicTerrainComponent.currentPhysicsCenter;
-      const correctedAggregateCenter = new Vector3(center.x, center.y, center.z);
-      correctedAggregateCenter.x -= MAX_VIEW_DISTANCE;
-      correctedAggregateCenter.z -= MAX_VIEW_DISTANCE;
-      // get the distance from the camera to the center of the physics aggregate in the xz plane
+      const center = dynamicTerrainComponent.currentPhysicsCenter.clone();
+      const dynamicTerrainPosition = dynamicTerrainComponent.dynamicTerrain?.mesh.getAbsolutePosition().clone();
+      if (!dynamicTerrainPosition) {
+        return;
+      }
+
+      // get the distance from the locomtoive to the center of the physics aggregate in the xz plane
       const distance = Vector3.Distance(
-        new Vector3(camera.globalPosition.clone().x, 0, camera.globalPosition.clone().z),
-        new Vector3(correctedAggregateCenter.x, 0, correctedAggregateCenter.z),
+        new Vector3(locomotive.getAbsolutePosition().clone().x, 0, locomotive.getAbsolutePosition().clone().z),
+        new Vector3(center.x, 0, center.z),
       );
       const centerMarker = scene.getMeshByName('centerMarker') as Mesh;
-      centerMarker.position = correctedAggregateCenter;
-      console.log(correctedAggregateCenter, camera.globalPosition.clone(), distance);
-      if (distance > RADIUS_OF_PHYSICS_AGGREGATE / 2 - 1) {
+      centerMarker.position = center.clone();
+      console.log(center, locomotive.getAbsolutePosition().clone(), distance);
+      if (distance > DIAMETER_OF_PHYSICS_AGGREGATE / 2 - 1) {
         console.log('making new physics aggregate');
         dynamicTerrainComponent.physicsAggregateInitializing = true;
         const previousPhysicsMesh = dynamicTerrainComponent.physicsMesh;
-        const result = this.createPhysicsAggregate(terrainMesh, camera.globalPosition.clone(), RADIUS_OF_PHYSICS_AGGREGATE);
+        const result = this.createPhysicsAggregate(terrainMesh, locomotive.getAbsolutePosition().clone(), DIAMETER_OF_PHYSICS_AGGREGATE);
         dynamicTerrainComponent.physicsMesh = result.physicsMeshClone;
         dynamicTerrainComponent.currentPhysicsCenter = result.centerPosition;
         previousPhysicsMesh?.dispose();
@@ -81,19 +72,18 @@ export class DynamicTerrainColliderSystem extends IterativeSystem {
 
   private createPhysicsAggregate(
     terrainMesh: Mesh,
-    cameraPosition: Vector3,
-    radius: number,
+    playerPosition: Vector3,
+    diameter: number,
   ): { physicsMeshClone: Mesh; centerPosition: Vector3 } {
-    // Create an empty array to store the vertices and indices within the specified radius
-    const verticesWithinRadius: number[] = [];
-    const indicesWithinRadius: number[] = [];
-
-    cameraPosition.x += MAX_VIEW_DISTANCE;
-    cameraPosition.z += MAX_VIEW_DISTANCE;
+    // Create an empty array to store the vertices and indices within the specified diameter
+    const verticesWithinDiameter: number[] = [];
+    const indicesWithinDiameter: number[] = [];
 
     // Get the vertices positions and indices from the original mesh
     const positions = terrainMesh.getVerticesData(VertexBuffer.PositionKind);
     const indices = terrainMesh.getIndices();
+
+    const worldMatrix = terrainMesh.getWorldMatrix();
 
     let sumX = 0,
       sumY = 0,
@@ -101,58 +91,65 @@ export class DynamicTerrainColliderSystem extends IterativeSystem {
     let count = 0;
 
     if (positions && indices) {
-      // Create a map to store the new indices for the vertices within the radius
+      // Create a map to store the new indices for the vertices within the diameter
       const indexMap = new Map<number, number>();
 
-      // Iterate through the indices to find the vertices within the specified radius
+      // Iterate through the indices to find the vertices within the specified diameter
       for (let i = 0; i < indices.length; i += 3) {
         const index1 = indices[i];
         const index2 = indices[i + 1];
         const index3 = indices[i + 2];
 
-        const position1 = new Vector3(positions[index1 * 3], positions[index1 * 3 + 1], positions[index1 * 3 + 2]);
-        const position2 = new Vector3(positions[index2 * 3], positions[index2 * 3 + 1], positions[index2 * 3 + 2]);
-        const position3 = new Vector3(positions[index3 * 3], positions[index3 * 3 + 1], positions[index3 * 3 + 2]);
+        const localPosition1 = new Vector3(positions[index1 * 3], positions[index1 * 3 + 1], positions[index1 * 3 + 2]);
+        const localPosition2 = new Vector3(positions[index2 * 3], positions[index2 * 3 + 1], positions[index2 * 3 + 2]);
+        const localPosition3 = new Vector3(positions[index3 * 3], positions[index3 * 3 + 1], positions[index3 * 3 + 2]);
 
-        const distance1 = Vector3.Distance(new Vector3(cameraPosition.x, 0, cameraPosition.z), new Vector3(position1.x, 0, position1.z));
-        const distance2 = Vector3.Distance(new Vector3(cameraPosition.x, 0, cameraPosition.z), new Vector3(position2.x, 0, position2.z));
-        const distance3 = Vector3.Distance(new Vector3(cameraPosition.x, 0, cameraPosition.z), new Vector3(position3.x, 0, position3.z));
+        const position1 = Vector3.TransformCoordinates(localPosition1, worldMatrix);
+        const position2 = Vector3.TransformCoordinates(localPosition2, worldMatrix);
+        const position3 = Vector3.TransformCoordinates(localPosition3, worldMatrix);
 
-        if (distance1 <= radius && distance2 <= radius && distance3 <= radius) {
-          // Add the indices to the array if all three vertices of the face are within the radius
+        const distance1 = Vector3.Distance(new Vector3(playerPosition.x, 0, playerPosition.z), new Vector3(position1.x, 0, position1.z));
+        const distance2 = Vector3.Distance(new Vector3(playerPosition.x, 0, playerPosition.z), new Vector3(position2.x, 0, position2.z));
+        const distance3 = Vector3.Distance(new Vector3(playerPosition.x, 0, playerPosition.z), new Vector3(position3.x, 0, position3.z));
+
+        if (distance1 <= diameter && distance2 <= diameter && distance3 <= diameter) {
+          // Add the indices to the array if all three vertices of the face are within the diameter
           [index1, index2, index3].forEach((originalIndex) => {
             if (!indexMap.has(originalIndex)) {
-              // Add a new index for the vertex within the radius
-              const newIndex = verticesWithinRadius.length / 3;
+              // Add a new index for the vertex within the diameter
+              const newIndex = verticesWithinDiameter.length / 3;
               indexMap.set(originalIndex, newIndex);
-              verticesWithinRadius.push(positions[originalIndex * 3], positions[originalIndex * 3 + 1], positions[originalIndex * 3 + 2]);
+              verticesWithinDiameter.push(localPosition1.x, localPosition1.y, localPosition1.z);
+              verticesWithinDiameter.push(localPosition2.x, localPosition2.y, localPosition2.z);
+              verticesWithinDiameter.push(localPosition3.x, localPosition3.y, localPosition3.z);
 
-              sumX += positions[originalIndex * 3];
-              sumY += positions[originalIndex * 3 + 1];
-              sumZ += positions[originalIndex * 3 + 2];
-              count++;
+              sumX += localPosition1.x + localPosition2.x + localPosition3.x;
+              sumY += localPosition1.y + localPosition2.y + localPosition3.y;
+              sumZ += localPosition1.z + localPosition2.z + localPosition3.z;
+              count += 3;
             }
-            const indexWithinRadius = indexMap.get(originalIndex);
-            if (indexWithinRadius !== undefined) {
-              indicesWithinRadius.push(indexWithinRadius);
+            const indexWithinDiameter = indexMap.get(originalIndex);
+            if (indexWithinDiameter !== undefined) {
+              indicesWithinDiameter.push(indexWithinDiameter);
             }
           });
         }
       }
     }
 
-    // Create a new mesh with the vertices and indices within the specified radius
+    // Create a new mesh with the vertices and indices within the specified diameter
     const physicsMeshClone = new Mesh('terrainPhysicsMesh', scene);
     const vertexData = new VertexData();
-    vertexData.positions = verticesWithinRadius;
-    vertexData.indices = indicesWithinRadius;
+    vertexData.positions = verticesWithinDiameter;
+    vertexData.indices = indicesWithinDiameter;
     vertexData.applyToMesh(physicsMeshClone);
     physicsMeshClone.position = terrainMesh.position.clone();
 
     // Create the physics aggregate for the new mesh
     const physicsAggregate = new PhysicsAggregate(physicsMeshClone, PhysicsShapeType.MESH, { mass: 0 });
     physicsMeshClone.setEnabled(false);
-    const centerPosition = new Vector3(sumX / count, sumY / count, sumZ / count);
+    let centerPosition = new Vector3(sumX / count, sumY / count, sumZ / count);
+    centerPosition = Vector3.TransformCoordinates(centerPosition, worldMatrix);
     return { physicsMeshClone, centerPosition };
   }
 }
